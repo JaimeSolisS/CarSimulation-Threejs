b2292602cb8c1bab08727ad4f1bebd937e78990c
JaimeSolisS <jaimesolissoto@me.com>
2021-02-02 16:04:51 -0700

    Feat load city and car

---
+++ public/v4/app.js
@@ -0,0 +1,65 @@
+class App {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        //CREATE SCENE
+        this.scene = new THREE.Scene();
+
+        //CREATE CAMERA
+        this.camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 100000);
+        this.camera.position.set(300, 200, 500);
+
+        //CREATE RENDERER
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
+
+        //SET LIGHTS
+        const light = new THREE.DirectionalLight("rgb(255, 255, 255)");
+        light.position.set(300, 300, 300);
+
+        /*
+        const directionalLightHelper = new THREE.DirectionalLightHelper(light);
+        this.scene.add(directionalLightHelper);
+        */
+
+        const ambient = new THREE.AmbientLight("rgb(0, 112, 112)", 0.5);
+        this.scene.add(light);
+        this.scene.add(ambient);
+
+        //SPIN CAMERA
+        this.controls = new THREE.OrbitControls(this.camera);
+
+        //LOADER
+        const loader = new THREE.FBXLoader();
+        const app = this;
+
+        //load method (fbx file path, load method (what we want to do once the objetc is loaded), progress as it is downloaded, error)
+        loader.load("../assets/cityCar.fbx", function(object) {
+            app.car = object;
+            app.scene.add(object);
+            //camera moves with mouse control
+            app.controls.target = object.position.clone();
+            app.controls.update();
+
+            object.traverse(function(child) {
+                if (child.isMesh) {
+                    child.castShadow = child.receiveShadow = true;
+                }
+            });
+            app.animate();
+
+        }, null, function(error) {
+            console.error(error);
+        })
+    }
+
+    animate() {
+        const app = this;
+        requestAnimationFrame(function() { app.animate(); });
+
+        this.renderer.render(this.scene, this.camera);
+    }
+}
